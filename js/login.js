577a2da80d2d68bd3e37433596f7668fff918f59
co-ena87 <co.ena87@gmail.com>
2025-07-22 16:12:33 +0900

    🧾 메인화면 구축 → html+css 블록 단위 구성 / 내부 디테일 구조화 중 (7/22)

--- js/login.js
+++ js/login.js
@@ -3,4 +3,4 @@ const loginbtn =document.getElementById('login_btn');
 onClick = () => {
     window.location.href = 'resite.html'
 };
-loginbtn.addEventListener('click', onClick)
+loginbtn.addEventListener('click', onClick);
