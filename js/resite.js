2270e6c14099829213517fc620708e00615419b6
co-ena87 <co.ena87@gmail.com>
2025-11-17 20:26:03 +0900

    기존 nav 코드폐기. 메인 화면인 영수증 화면에 더 집중할 수 있도록 nav 바 자체는 깔끔하게 만들기로하고 nav바 기본 구성은 완료 추후 클릭 css 추가예정 , 스코어 점수 보여주는 div생성

--- js/resite.js
+++ js/resite.js
@@ -1,30 +1,23 @@
-
-// edit.html 페이지로 이동 
-const resite = document.getElementById('new-res-go');
+// edit.html 페이지로 이동
+const resite = document.getElementById("new-res-go");
 
 clicknext = () => {
-    window.location.href='edit.html'
+  window.location.href = "edit.html";
 };
-resite.addEventListener('click', clicknext);
+resite.addEventListener("click", clicknext);
 
 //별점 스코어
 
-const stars = document.querySelectorAll('#star-icons i');
+const stars = document.querySelectorAll("#star-icons i");
 let rating = 0;
 
 function setRating(n) {
-    rating = n;
-    stars.forEach((elm, i ) => {
-        elm.classList.toggle('active', i < n)
-    });
-};
-    stars.forEach((elm , idx) => {
-        // 마우스클릭
-        elm.addEventListener('click', () => setRating(idx + 1));
-    
-    });
-
-
-
-
-
+  rating = n;
+  stars.forEach((elm, i) => {
+    elm.classList.toggle("active", i < n);
+  });
+}
+stars.forEach((elm, idx) => {
+  // 마우스클릭
+  elm.addEventListener("click", () => setRating(idx + 1));
+});
