cb1f43c1285257d56c66c31b2bc0ef40f7b69062
co-ena87 <co.ena87@gmail.com>
2025-11-24 21:19:34 +0900

    nav바는 완전히 완성하였고 배우 파트에서 아이콘 클릭시 모달창 띄우기 아이콘 선택 하기 기능 추가 하기로함 모달창은 html <dialog>태그 선택해서 기본구조 구축하였음 지금은 모달 속 아이콘 정렬하고 있었는데 계속 의도한 위치로 이동을 안해서 html 구조가 잘 못되었는지 다시 확인중...

--- js/resite.js
+++ js/resite.js
@@ -1,23 +1,22 @@
-// edit.html 페이지로 이동
-const resite = document.getElementById("new-res-go");
-
-clicknext = () => {
-  window.location.href = "edit.html";
-};
-resite.addEventListener("click", clicknext);
-
 //별점 스코어
 
 const stars = document.querySelectorAll("#star-icons i");
+const userScore = document.getElementById("user-score");
 let rating = 0;
 
 function setRating(n) {
   rating = n;
+
+  //별 active
   stars.forEach((elm, i) => {
     elm.classList.toggle("active", i < n);
   });
+
+  userScore.textContent = rating;
 }
 stars.forEach((elm, idx) => {
   // 마우스클릭
   elm.addEventListener("click", () => setRating(idx + 1));
 });
+
+console.log(userScore);
