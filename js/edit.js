9237d3da9f5e03d3e9d9e75ac99720d21c6f6e64
co-ena87 <co.ena87@gmail.com>
2025-12-26 22:03:55 +0900

    잡다한 css 정리 edite.html 버튼 2개 추가

---
+++ js/edit.js
@@ -0,0 +1,75 @@
+//별점 스코어
+const stars = document.querySelectorAll("#star-icons i");
+const userScore = document.getElementById("user-score");
+let rating = 0;
+
+function setRating(n) {
+  rating = n;
+
+  //별 active
+  stars.forEach((elm, i) => {
+    elm.classList.toggle("active", i < n);
+  });
+
+  userScore.textContent = rating;
+}
+stars.forEach((elm, idx) => {
+  // 마우스클릭
+  elm.addEventListener("click", () => setRating(idx + 1));
+});
+
+//클릭이벤트
+
+const chariconModal = document.querySelector("dialog");
+
+let nowicon = ""; // actor-card 의 처음 기본값이 담긴 변수
+let moadlicon = ""; // modalicon안에 아이콘 클릭시 정보
+
+const iconclick = document.querySelectorAll(".actor-card-click");
+iconclick.forEach((card) => {
+  card.addEventListener("click", () => {
+    const cardEl = card.querySelector("i");
+
+    nowicon = {
+      cardEl: card, //카드 div
+      index: Number(card.dataset.index), // 카드순서
+      iconEl: cardEl, // i태그
+      iconType: card.dataset.icon, // i 어떤 표정인지
+      iconClass: cardEl.className, // 현재 클라스이름
+    };
+
+    chariconModal.showModal();
+  });
+});
+
+const modaInclick = document.querySelectorAll(".icons-op i");
+modaInclick.forEach((modal) => {
+  modal.addEventListener("click", (event) => {
+    if (!event.target.matches("i")) return;
+    if (!nowicon) {
+      alert("배우 아이콘 선택해주세요.");
+    }
+
+    const modalEl = event.target;
+
+    moadlicon = {
+      modadEl: modal, //카드 div
+      iconType: modal.dataset.icon, // i 어떤 표정인지
+      iconClass: modalEl.className, // 현재 클라스이름
+    };
+
+    //카드의 i를 모달에서 고른 아이콘으로 교체
+    // 카드 쪽에 fa-2x 유지
+    nowicon.iconEl.className = `${moadlicon.iconClass} fa-2x`;
+
+    // moadl 아이콘 선택 후 nowicon iconType 프로퍼티 변경 적용
+    nowicon.cardEl.dataset.icon = moadlicon.iconType;
+
+    nowicon.iconType = moadlicon.iconType;
+    nowicon.iconClass = nowicon.iconEl.className;
+
+    chariconModal.close();
+  });
+});
+
+// edit.js 저장버튼
--- js/preview.init.js
+++
@@ -1,9 +0,0 @@
-// 프리뷰: 보기 전용
-document.addEventListener('DOMContentLoaded', () => {
-  StarRating.mount('.star-score', {
-    storageKey: 'resite:v1',
-    field: 'rating',
-    interactive: false,  // 조작 X
-    step: 0.5
-  });
-});
